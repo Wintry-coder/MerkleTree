df9c870a1dfb39535e737e4f6e8ad8dc9ac5dd75
Wintry-coder <1332765311@qq.com>
2020-12-24 15:48:26 +0800

    -

--- MerkleTree.cpp
+++ MerkleTree.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
-
-void quicksort(int A[],int l,int r){
-   int m=l;
-   for(int i=l;i<r;i++){
-      if(A[i]<A[r]){
-         swap(A[i],A[m]);
-         ++m;
-      }
-   }
-   swap(A[m],A[r]);
-   if(m>l+1)quicksort(A,l,m-1);
-   if(m<r-1)quicksort(A,m+1,r);
-}
-
-int main(){
-   int A[10];
-   for(int i=0;i<10;i++) A[i]=10-i;
-   for(int i=0;i<10;i++) cout<<A[i]<<' ';
-   cout<<endl;
-   quicksort(A,0,10-1);
-   for(int i=0;i<10;i++) cout<<A[i]<<' ';
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+ 
+int main() {
+   
+   unsigned char digest[SHA_DIGEST_LENGTH];
+   const char* string = "hello world";
+   
+   SHA_CTX ctx;
+   SHA1_Init(&ctx);
+   SHA1_Update(&ctx, string, strlen(string));
+   SHA1_Final(digest, &ctx);
+   
+   char mdString[SHA_DIGEST_LENGTH*2+1];
+   for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+   
+   sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+   
+   printf("SHA1 digest: %s\n", mdString);
+   
+   return 0;
+ 
 }
