324d9487ff0823b0bee52a2613ccc05ebfa9d6f2
Wintry-coder <1332765311@qq.com>
2020-12-26 10:48:15 +0800

    -

--- MerkleTree.cpp
+++ MerkleTree.cpp
@@ -4,38 +4,46 @@ using namespace std;
 
 int main() {
    int n = 0;
-   cout << "请输入一个正数，用于初始化数据块的个数：";
+   cout << "Please input a positive number to initialize the number of data blocks: ";
    while(cin>>n){
       if(n <= 0)
-         cout << "输入不正确，请重新输入！\n请输入一个正数，用于初始化数据块的个数：";
+         cout << "Wrong input, please input again！\nPlease input a positive number to initialize the number of data blocks: ";
       else
          break;
    }
-   cout << "请输入每个数据块的数据字符串：\n";
+   cout << "Please enter the data string for each data block:\n";
    vector<char[65]>buff(n);
    vector<char*>data(n);
    string s;
+   getchar();
    for(int i = 1; i <= n; i++){
-      printf("第%d个：",i);
-      cin>>s;
-      char *p=(char*)s.c_str();
-      calSHA256(p,buff[i-1]);
-      data[i-1] = buff[i-1];
-      cout<<"该数据块对应的SHA256哈希值为:"<<buff[i-1]<<endl;
+      cout<< i <<": ";
+      getline(cin, s);
+      // char *p=(char*)s.c_str();
+      // calSHA256(p,buff[i-1]);
+      // data[i-1] = buff[i-1];
+      // cout<<"The SHA256 hash value corresponding to the data block: "<<buff[i-1]<<endl;
+      string hashs = SHA256(s);
+      cout<<"The SHA256 hash value corresponding to the data block: "<< hashs <<endl;
+
    }
-   MerkleTree mt = MerkleTree(data);
-   printf("root: %s\n",mt.root());
+   // MerkleTree mt = MerkleTree(data);
+   // printf("root: %s\n",mt.root());
    int op = 0;
-   while(1)
+/*   while(1)
    {
-      cout<<"请输入操作：\n";
-      cout<<"1.更新数据块 2.插入数据块 3.删除数据块 4.检索数据块 5.打印MerkleTree 0.退出\n";
+      cout<<"Please choose option 0-5：\n";
+      cout<<"1. Update data block 2. Insert data block 3. Delete data block 4. Retrieve data block 5. Print MerkleTree 0. Exit\n";
       cin>>op;
+      int index = 0;
       if(op == 0)
          break;
       switch (op)
       {
       case 1:
+         cout<<"Please input the index of data block you want to uodate: ";
+         cin>>index;
+         mt.UpdateMerkleTree(index);
          break;
       case 2:
          break;
@@ -46,11 +54,11 @@ int main() {
       case 5:
          break;    
       default:
-         cout<<"请正确输入！";
+         cout<<"Please input correctly!\n";
          break;
       }
       
-   }
+   }*/
    return 0;
  
 }
--- MerkleTree.h
+++ MerkleTree.h
@@ -5,6 +5,15 @@
 #include <string.h>
 using namespace std;
 
+string SHA256(string data) {
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  SHA256_Init(&sha256);
+  SHA256_Update(&sha256, data.data(), data.size());
+  SHA256_Final(hash, &sha256);
+  return string((char*)hash);
+}
+
 void calSHA256(char* inp,char out_buff[65]){
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
@@ -42,20 +51,22 @@ public:
 
     MerkleTree(){}
     MerkleTree(vector<char*> leaves){
-      tree = InitMerkleTree(mergeCalSHA256,leaves);
+      tree = InitMerkleTree(leaves);
     }
 
     size_t size(){return tree.size();}
     char* root(){return tree[0];}
-    vector<char*> InitMerkleTree(void (*combineFn)(char*,char*,char*),vector<char*> leaves);
+    vector<char*> InitMerkleTree(vector<char*> leaves);
+    void InsertMerkleTree(char* leaf);
+    void UpdateMerkleTree(int index);
 
 };
 
-vector<char*> MerkleTree::InitMerkleTree(void (*combineFn)(char*,char*,char*),vector<char*> leaves){
+vector<char*> MerkleTree::InitMerkleTree(vector<char*> leaves){
     int nodeCount = leaves.size()*2 - 1;
     int delta = nodeCount - leaves.size();
     vector<char*> tree(nodeCount);//初始化结点
-    cout<<"开始初始化Merkle Tree"<<endl;
+    
     for(int i = 0 ;i<leaves.size();i++){
         tree[delta + i] = new char[65];
         memcpy(tree[delta + i],leaves[i],65);
@@ -66,11 +77,17 @@ vector<char*> MerkleTree::InitMerkleTree(void (*combineFn)(char*,char*,char*),ve
     while(idx > 0){
         int parent = (idx -1)/2;
         tree[parent] = new char[65];
-        combineFn(tree[idx-1],tree[idx],tree[parent]);//相邻两个hash块串联，然后做hash运算
+        mergeCalSHA256(tree[idx-1],tree[idx],tree[parent]);//相邻两个hash块串联，然后做hash运算
         tree[parent][64] = 0;
         idx-=2;
     }
-
+    cout<<"Init Merkle Tree successfully!"<<endl;
     return tree;
-  }
+}
 
+void MerkleTree::InsertMerkleTree(char* leaf){
+    
+}
+void MerkleTree::UpdateMerkleTree(int index){
+
+}
