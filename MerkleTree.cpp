d8243fc910d7654ecacc7ffbb19e88f22cf0ed82
Wintry-coder <1332765311@qq.com>
2020-12-27 16:38:15 +0800

    -

--- MerkleTree.cpp
+++ MerkleTree.cpp
@@ -1,5 +1,5 @@
 #include "MerkleTree.h"
-
+unsigned char* filter;
 using namespace std;
 
 int main() {
@@ -12,54 +12,90 @@ int main() {
          break;
    }
    cout << "Please enter the data string for each data block:\n";
-   vector<char[65]>buff(n);
-   vector<char*>data(n);
+   filter = malloc(FILTER_SIZE_BYTES);
+   vector<string> data;
    string s;
    getchar();
    for(int i = 1; i <= n; i++){
       cout<< i <<": ";
       getline(cin, s);
-      // char *p=(char*)s.c_str();
-      // calSHA256(p,buff[i-1]);
-      // data[i-1] = buff[i-1];
-      // cout<<"The SHA256 hash value corresponding to the data block: "<<buff[i-1]<<endl;
-      string hashs = SHA256(s);
-      cout<<"The SHA256 hash value corresponding to the data block: "<< hashs <<endl;
-
+      data.push_back(s);
    }
-   // MerkleTree mt = MerkleTree(data);
-   // printf("root: %s\n",mt.root());
+   MerkleTree tree = MerkleTree(data);
+   cout<<"Init Merkle Tree successfully!"<<endl;   
    int op = 0;
-/*   while(1)
+   while(1)
    {
       cout<<"Please choose option 0-5：\n";
-      cout<<"1. Update data block 2. Insert data block 3. Delete data block 4. Retrieve data block 5. Print MerkleTree 0. Exit\n";
+      cout<<"1. Update data block 2. Insert data block 3. Delete data block 4. Retrieve data block 0. Exit\n";
       cin>>op;
       int index = 0;
+      string ssd;
+      bool enable_bf = false;
       if(op == 0)
          break;
       switch (op)
       {
       case 1:
-         cout<<"Please input the index of data block you want to uodate: ";
+         cout<<"Please input the index of data block you want to update( index starts from 1 ): ";
          cin>>index;
-         mt.UpdateMerkleTree(index);
+         getchar();
+         cout << "Please enter the data string for the data block:\n";
+         getline(cin, s);
+         tree.UpdateMerkleTree(index,s);
          break;
       case 2:
+         cout<<"Please input a positive number to represent the number of data blocks you want to insert: ";
+         while(cin>>n){
+            if(n <= 0)
+               cout << "Wrong input, please input again！\nPlease input a positive number to initialize the number of data blocks: ";
+            else
+               break;
+         }
+         getchar();
+         cout << "Please enter the data string for each data block:\n";
+         for(int i = 1; i <= n; i++){
+            cout<< i <<": ";
+            getline(cin, s);
+            data.push_back(s);
+         }         
+         tree = MerkleTree(data);
          break;
       case 3:
+         cout << "Please enter the data string you want delete:\n";
+         getchar();
+         getline(cin, ssd);
+         for(auto iter = data.begin();iter!=data.end();iter++){
+            if(*iter == ssd){
+               iter = data.erase(iter);
+               iter--;
+            }
+         }
+         tree = MerkleTree(data);
          break;
       case 4:
+         cout << "Please choose whether to use bloom filter(y or n): ";
+         cin>>ssd;
+         if(ssd == "y")
+         {
+            enable_bf = true;
+            cout<<"Bloom Filter start!\n";
+         }
+         getchar();
+         cout << "Please enter the data string you want retrieve:\n";
+         getline(cin, ssd);
+         if(tree.DataProof(ssd,enable_bf))
+            cout<<"Success!\n";
+         else
+            cout<<"Failed!\n";
          break; 
-      case 5:
-         break;    
       default:
          cout<<"Please input correctly!\n";
          break;
       }
       
-   }*/
+   }
+   free(filter);
    return 0;
- 
 }
 
--- MerkleTree.h
+++ MerkleTree.h
@@ -1,8 +1,13 @@
+#ifndef MERKLE_TREE_H_
+#define MERKLE_TREE_H_
+
 #include <iostream>
 #include <vector>
 #include <openssl/sha.h>
 #include <algorithm>
+#include <assert.h>
 #include <string.h>
+#include "bloom_filter.h"
 using namespace std;
 
 string SHA256(string data) {
@@ -13,81 +18,138 @@ string SHA256(string data) {
   SHA256_Final(hash, &sha256);
   return string((char*)hash);
 }
-
-void calSHA256(char* inp,char out_buff[65]){
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, inp, strlen(inp));
-    SHA256_Final(hash, &sha256);
-
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-      sprintf(out_buff + (i * 2), "%02x", hash[i]);
-    }
-    out_buff[64] = 0;
-}
-
-static void mergeCalSHA256(char* leftData,char* rightData,char out_buff[65]){
-
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256,leftData,strlen((const char*)leftData) );
-    SHA256_Update(&sha256,rightData,strlen((const char*)rightData) );
-    SHA256_Final(hash, &sha256);
-
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-      sprintf(out_buff + (i * 2), "%02x", hash[i]);
-    }
-    out_buff[64] = 0;
-}
-
+struct DataHashNode
+{
+    DataHashNode *left;
+    DataHashNode *right;
+    DataHashNode *parent;
+    string hash_;
+    DataHashNode():left(nullptr),right(nullptr),parent(nullptr),hash_(""){}
+    DataHashNode(DataHashNode* _left,DataHashNode* _right,DataHashNode* _parent,string _hash):left(_left),
+    right(_right),parent(_parent),hash_(_hash){}
+};
 
 class MerkleTree{
 public:
-    vector<char*> tree;
+    DataHashNode* root;
+    vector<DataHashNode*> leaves;
 
     MerkleTree(){}
-    MerkleTree(vector<char*> leaves){
-      tree = InitMerkleTree(leaves);
+    MerkleTree(vector<string> data){
+        DataHashNode* leaf;
+        for(int i = 0; i < data.size(); i++) {
+            leaf = new DataHashNode(nullptr,nullptr,nullptr,SHA256(data[i]));
+            leaves.push_back(leaf);
+        }
+        // 叶子节点奇数个，复制最后一个节点
+        if(data.size()%2 == 1) {
+            leaf = new DataHashNode(nullptr,nullptr,nullptr,SHA256(data[data.size()-1]));
+            leaves.push_back(leaf);
+        }
+        root = InitMerkleTree(leaves);
+        
     }
+    DataHashNode* InitMerkleTree(vector<DataHashNode*> &nodes);
+    bool DataProof(string data, int enable_bloom_filter);
+    DataHashNode* findLeaf(string str);
 
-    size_t size(){return tree.size();}
-    char* root(){return tree[0];}
-    vector<char*> InitMerkleTree(vector<char*> leaves);
-    void InsertMerkleTree(char* leaf);
-    void UpdateMerkleTree(int index);
+    void UpdateMerkleTree(int index, string str);
 
 };
 
-vector<char*> MerkleTree::InitMerkleTree(vector<char*> leaves){
-    int nodeCount = leaves.size()*2 - 1;
-    int delta = nodeCount - leaves.size();
-    vector<char*> tree(nodeCount);//初始化结点
-    
-    for(int i = 0 ;i<leaves.size();i++){
-        tree[delta + i] = new char[65];
-        memcpy(tree[delta + i],leaves[i],65);
-        tree[delta + i][64] = 0;
+DataHashNode* MerkleTree::InitMerkleTree(vector<DataHashNode*> &nodes){
+    if(nodes.size() == 1) 
+        return nodes[0];
+    vector<DataHashNode*> temp;
+    DataHashNode* tmp, *left, *right;
+
+    string hash_tmp;
+    for(int i = 0; i < nodes.size(); i += 2) {
+        left = nodes[i];
+        if(i+1 == nodes.size()) {
+            tmp = new DataHashNode(left, nullptr, nullptr, left->hash_);
+            temp.push_back(tmp);
+            left->parent = tmp;// 内部节点奇数个，只对left节点进行计算
+        }
+        else {
+            right = nodes[i+1];
+            string temp_hash = left->hash_ + right->hash_;
+            hash_tmp = SHA256(temp_hash);
+            tmp = new DataHashNode(left, right, nullptr, hash_tmp);
+            temp.push_back(tmp);
+            left->parent = tmp;
+            right->parent = tmp;
+        }
     }
+    return InitMerkleTree(temp);
+}
 
-    int idx = nodeCount-1;
-    while(idx > 0){
-        int parent = (idx -1)/2;
-        tree[parent] = new char[65];
-        mergeCalSHA256(tree[idx-1],tree[idx],tree[parent]);//相邻两个hash块串联，然后做hash运算
-        tree[parent][64] = 0;
-        idx-=2;
+bool MerkleTree::DataProof(string data, int enable_bloom_filter){
+    string hashstr = SHA256(data);
+    DataHashNode* leaf;
+    if(enable_bloom_filter){
+
+    }else{
+        leaf = findLeaf(hashstr);
     }
-    cout<<"Init Merkle Tree successfully!"<<endl;
-    return tree;
+    if(leaf == nullptr)
+        return false;
+    DataHashNode* current_parent = leaf->parent;
+    string root_hash = leaf->hash_;
+    string combo_hash;
+    while(current_parent != nullptr) {
+        if(current_parent->left == leaf) {
+            if(current_parent->right){
+                combo_hash = root_hash + current_parent->right->hash_;
+                root_hash = SHA256(combo_hash);
+            }
+        }
+        else {
+            combo_hash = current_parent->left->hash_ + root_hash;
+            root_hash = SHA256(combo_hash);
+        }
+        leaf = current_parent;
+        current_parent = current_parent->parent;
+    }
+    if(root_hash == root->hash_) 
+        return true;
+    return false;    
 }
 
-void MerkleTree::InsertMerkleTree(char* leaf){
-    
-}
-void MerkleTree::UpdateMerkleTree(int index){
 
+DataHashNode* MerkleTree::findLeaf(string str) {
+    for(int i = 0; i< leaves.size(); i++) {
+        if(str == leaves[i]->hash_) {
+            return leaves[i];
+        }
+    }
+    return nullptr;
 }
+
+void MerkleTree::UpdateMerkleTree(int index, string str){
+    string hash_ = SHA256(str);
+    if(index > leaves.size()||index <= 0){
+        cout<<"Wrong index\n";
+        return;
+    }
+    DataHashNode* leaf = leaves[index-1];
+    leaf->hash_ = hash_;
+    DataHashNode* current_parent = leaf->parent;
+    string combo_hash;
+    while(current_parent != nullptr) {
+        if(current_parent->left == leaf) {
+            if(current_parent->right){
+                combo_hash = hash_ + current_parent->right->hash_;
+                hash_ = SHA256(combo_hash);
+            }
+        }
+        else {
+            combo_hash = current_parent->left->hash_ + hash_;
+            hash_ = SHA256(combo_hash);
+        }
+        current_parent->hash_ = hash_;
+        leaf = current_parent;
+        current_parent = current_parent->parent;
+    }    
+}
+#endif
